1b3e8af55c75570a30942fbe5deb44a220cdb739
WeizenJoe <weizenjoe@posteo.de>
2014-11-22 21:29:57 +0100

    Initial commit of some helpers

---
+++ import_helpers/remove_already_known_files.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2.7
+
+import argparse
+import logging
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
+import pydb.pyrosetup
+
+logger = logging.getLogger("rakf")
+
+db = pydb.pyrosetup.pydbserver()
+
+
+def already_known_book(filepath):
+    base, extension = os.path.splitext(filepath)
+    extension = extension[1:]  # remove dot
+
+    abspath = os.path.abspath(filepath)
+    try:
+        return db.is_local_file_known(abspath, extension)
+    except ValueError:
+        print "Ex"
+        return False
+
+
+def remove_already_known_ebooks(path, dry_run=False):
+    check_count = 0
+    remove_count = 0
+
+    def log_progress():
+        logger.info("%d/%d removed %0.1f %%" % (remove_count, check_count, remove_count * 100.0 / check_count ))
+
+    for dir_name, dirs, files in os.walk(path):
+        for f in files:
+            _, f_ext = os.path.splitext(f)
+            if f_ext.lower() == '.opf':
+                continue
+
+            filepath = os.path.join(dir_name, f)
+            logger.debug("Checking {}".format(filepath))
+            check_count += 1
+
+            if already_known_book(filepath):
+                if dry_run:
+                    logger.info("Would remove {}".format(filepath))
+                else:
+                    logger.info("Removing {}".format(filepath))
+                    os.remove(filepath)
+
+                remove_count += 1
+
+            if check_count % 10 == 0:
+                log_progress()
+
+    if check_count > 0 and check_count % 10 != 0:
+        log_progress()
+
+
+parser = argparse.ArgumentParser(description='Removes files that are already known to the database')
+parser.add_argument('paths', help='Path to file or directory to scan', nargs='+')
+parser.add_argument('-d', '--debug', help='Enables debug output', action='store_true')
+parser.add_argument('-n', '--dry-run', help='Do not really delete files', action='store_true')
+
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO, format="%(name)8s: %(message)s")
+
+for path in args.paths:
+    remove_already_known_ebooks(path, args.dry_run)
+
+
+
