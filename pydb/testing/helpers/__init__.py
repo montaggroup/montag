ad2043830fd8ef825dbf4819cde3b3884d999d7e
dsk7 <jensg@posteo.de>
2015-01-18 14:09:01 +0100

    factored out in-memory database creation

---
+++ pydb/testing/helpers/__init__.py
@@ -0,0 +1,28 @@
+import logging
+
+import pydb.localdb
+import pydb.friendsdb
+import pydb.mergedb
+import pydb.foreigndb
+import pydb.maindb
+
+import mock
+
+logger = logging.getLogger('testing.helpers')
+
+def build_main_db_memory_only(schema_path):
+    """ builds a database with all subdatabases in memory """
+    local_db = pydb.localdb.LocalDB(":memory:", schema_path, enable_db_sync=False)
+    merge_db = pydb.mergedb.MergeDB(":memory:", schema_path, enable_db_sync=False)
+    merge_db.add_source(local_db)
+    friends_db = pydb.friendsdb.FriendsDB(":memory:", schema_path)
+    index_server = mock.MagicMock()
+
+    def build_foreign_db(friend_id):
+        db = pydb.foreigndb.ForeignDB(":memory:", schema_path, friend_id, enable_db_sync=False)
+        return db
+
+    db = pydb.maindb.MainDB(local_db, friends_db, merge_db, build_foreign_db, index_server)
+
+    logger.info("DBs initialized")
+    return db
