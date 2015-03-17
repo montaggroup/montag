396301e2312f484281fbb86e4a18989d0ccc31c4
dsk7 <jensg@posteo.de>
2015-03-18 00:50:41 +0100

    added test for tome_search execution

--- pydb/testing/frontend_tests/test_tome_search.py
+++ pydb/testing/frontend_tests/test_tome_search.py
@@ -22,6 +22,7 @@ def prepare_environment(controller_name):
 
 def start_services():
     pydb.services.start(pydb.services.service_name('pydbserver'))
+    pydb.services.start(pydb.services.service_name('indexserver'))
 
 
 def stop_services():
@@ -36,15 +37,40 @@ def dump_result(res):
 class TestTomeSearch(unittest.TestCase):
     def setUp(self):
         self.env = prepare_environment('default')
-        self.request = gluon.globals.Request({})
+        self.request = self.env['request']
+        self.response = self.env['response']
         start_services()
 
     def tearDown(self):
         stop_services()
 
-    def test_opening_search_form_returns_a_form_element(self):
+    def _add_args(self, *args):
+        for arg in args:
+            self.request.args.append(arg)
+
+    def _add_post_vars(self, var_dict):
+        self.request.vars.update(var_dict)
+        self.request.post_vars.update(var_dict)
+
+    def test_opening_search_form_returns_a_form_element_and_view_renders(self):
         res = self.env['tomesearch']()
         self.assertIn('form', res)
+
+        html = self.response.render('default/tomesearch.html', res)
+        self.assertIn('<form ', html)
+
+    def test_executed_search_from_returns_a_result_list_and_view_renders(self):
+        self._add_post_vars({
+            '_formname': 'search',
+            'query': 'hello',
+            'principal_language': 'en',
+            'tome_type': '2'
+        })
+        res = self.env['tomesearch']()
+        self.assertIn('tome_info', res)
+
+        html = self.response.render('default/tomesearch.html', res)
+        self.assertIn('Results for hello', html)
 
 
 if __name__ == '__main__':
