f7d0f4dcf0f8be0f5299fb64ae39edb090bff432
dsk7 <jensg@posteo.de>
2015-03-17 19:42:03 +0100

    began implementation of web2py tests

---
+++ pydb/testing/frontend_tests/test_tome_search.py
@@ -0,0 +1,47 @@
+import sys
+import os
+
+web2py_path = os.path.join('..', '..', '..', 'web2py')
+sys.path.append(os.path.realpath(web2py_path))
+
+# web2py likes to run with current dir = web2py path
+os.chdir(web2py_path)
+
+import unittest
+import gluon.globals
+import gluon.shell
+import pydb.services
+
+
+
+def prepare_environment(controller_name):
+    app_dir = os.path.join('applications', 'montag')
+    env = gluon.shell.env(app_dir, c='default', dir=app_dir, import_models=True)
+    execfile(os.path.join(app_dir, 'controllers', controller_name + '.py'), env)
+    return env
+
+
+def start_services():
+    pydb.services.start('montag-pydbserver')
+
+
+def stop_services():
+    pydb.services.stop('montag-pydbserver')
+
+
+class TestTomeSearch(unittest.TestCase):
+    def setUp(self):
+        self.env = prepare_environment('default')
+        self.request = gluon.globals.Request({})
+        start_services()
+
+    def tearDown(self):
+        stop_services()
+
+    def test_opening_search_form_works(self):
+        res = self.env['tomesearch']()
+        print res
+
+
+if __name__ == '__main__':
+    unittest.main()
