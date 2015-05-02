f6f0e034299c08369dbe2a092d4a15d7ffd25471
dsk7 <jensg@posteo.de>
2015-05-02 22:24:30 +0200

    fixed circular import

--- pydb/pyrosetup.py
+++ pydb/pyrosetup.py
@@ -5,15 +5,10 @@ Pyro4.config.SOCK_REUSE = True
 Pyro4.config.SERIALIZER = 'pickle'
 Pyro4.config.SERIALIZERS_ACCEPTED = ['serpent', 'json', 'marshal', 'pickle']
 
-import comservice as comservice_
-import maindb
-import commdatastore
-import indexserver as indexserver_
-import fileserver as fileserver_
-
 
 def pydbserver():
     if False:  # trick ide into using the correct code insight
+        import maindb
         return maindb.MainDB(None, None, None, None, None)
 
     return Pyro4.Proxy('PYRO:pydb_server@localhost:4510')
@@ -21,6 +16,7 @@ def pydbserver():
 
 def comm_data_store():
     if False:  # trick ide into using the correct code insight
+        import commdatastore
         return commdatastore.CommDataStore(None, None)
 
     return Pyro4.Proxy('PYRO:comm_data_store@localhost:4510')
@@ -28,6 +24,7 @@ def comm_data_store():
 
 def indexserver():
     if False:  # trick ide into using the correct code insight
+        import indexserver as indexserver_
         return indexserver_.IndexServer(None, None)
 
     return Pyro4.Proxy('PYRO:index_server@localhost:4512')
@@ -35,6 +32,7 @@ def indexserver():
 
 def comservice():
     if False:  # trick ide into using the correct code insight
+        import comservice as comservice_
         return comservice_.ComService(None)
 
     return Pyro4.Proxy('PYRO:comservice@localhost:4513')
@@ -42,6 +40,7 @@ def comservice():
 
 def fileserver():
     if False:  # trick ide into using the correct code insight
+        import fileserver as fileserver_
         return fileserver_.FileServer(None, None)
 
     return Pyro4.Proxy('PYRO:fileserver@localhost:4514')
