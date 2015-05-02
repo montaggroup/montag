eab3702cddfc3f1cce8834918263eee905e6feec
dsk7 <jensg@posteo.de>
2015-05-02 22:21:57 +0200

    added a delay into fetch_updates from all to avoid bug where no update actually starts

--- pydb/pyrosetup.py
+++ pydb/pyrosetup.py
@@ -5,23 +5,44 @@ Pyro4.config.SOCK_REUSE = True
 Pyro4.config.SERIALIZER = 'pickle'
 Pyro4.config.SERIALIZERS_ACCEPTED = ['serpent', 'json', 'marshal', 'pickle']
 
+import comservice as comservice_
+import maindb
+import commdatastore
+import indexserver as indexserver_
+import fileserver as fileserver_
+
 
 def pydbserver():
+    if False:  # trick ide into using the correct code insight
+        return maindb.MainDB(None, None, None, None, None)
+
     return Pyro4.Proxy('PYRO:pydb_server@localhost:4510')
 
 
 def comm_data_store():
+    if False:  # trick ide into using the correct code insight
+        return commdatastore.CommDataStore(None, None)
+
     return Pyro4.Proxy('PYRO:comm_data_store@localhost:4510')
 
 
 def indexserver():
+    if False:  # trick ide into using the correct code insight
+        return indexserver_.IndexServer(None, None)
+
     return Pyro4.Proxy('PYRO:index_server@localhost:4512')
 
 
 def comservice():
+    if False:  # trick ide into using the correct code insight
+        return comservice_.ComService(None)
+
     return Pyro4.Proxy('PYRO:comservice@localhost:4513')
 
 
 def fileserver():
+    if False:  # trick ide into using the correct code insight
+        return fileserver_.FileServer(None, None)
+
     return Pyro4.Proxy('PYRO:fileserver@localhost:4514')
 
