efc06885c33f8162df9021976bc2602ab4d0f870
dsk7 <jensg@posteo.de>
2015-10-25 10:50:04 +0100

    make pydbadd use name_key for deduplicating authors when adding new tomes

--- pydb/testing/integration_tests/test_author_add.py
+++ pydb/testing/integration_tests/test_author_add.py
@@ -13,7 +13,6 @@ logging.basicConfig(level=logging.INFO)
 class TestAddAuthor(unittest.TestCase):
     def setUp(self):
         self.main_db = helpers.build_main_db_memory_only(pydb.testing.guess_schema_dir())
-        self.friend_1_id = self.main_db.add_friend('a friend')
 
     def test_adding_an_author_generates_a_name_key(self):
         self.main_db.add_author(name='john Smith_')
@@ -48,6 +47,26 @@ class TestAddAuthor(unittest.TestCase):
         guid = item['guid']
         author_doc = self.main_db.get_local_author_document_by_guid(guid)
         self.assertEqual(author_doc['name'], 'john Smith')
+
+
+class TestFindOrCreateAuthor(unittest.TestCase):
+    def setUp(self):
+        self.main_db = helpers.build_main_db_memory_only(pydb.testing.guess_schema_dir())
+
+    def test_after_adding_a_author_find_or_create_add_author_won_t_add_another_one_having_the_same_name_key(self):
+        id1 = self.main_db.find_or_create_author(author_name='john w. Smith', fidelity=60)
+        id2 = self.main_db.find_or_create_author(author_name='john w smith', fidelity=60)
+        self.assertEqual(id1, id2)
+
+    def test_find_or_create_author_will_fall_back_to_exact_match_if_multiple_name_key_matches_exist(self):
+        id1 = self.main_db.add_author(name='john w. Smith', date_of_birth="123")
+        id2 = self.main_db.add_author(name='john w smith', date_of_birth="234")
+
+        id3 = self.main_db.find_or_create_author(author_name='john w smith', fidelity=60)
+
+        self.assertEqual(id2, id3)
+        self.assertNotEqual(id1, id3)
+
 
 if __name__ == '__main__':
     unittest.main()
