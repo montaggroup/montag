e768041ff22e5bbab03633b4a275762e57f50cf6
dsk7 <jensg@posteo.de>
2020-04-19 21:02:37 +0200

    fix path name

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /srv/montag/books/{file_store,import_watch}
-mkdir -p /srv/montag/metadata/{db,config,logs,web2py_db}
+mkdir -p /srv/montag/metadata/{db,config,logs,web2py_databases}
 
 if [ ! -f /srv/montag/metadata/config/pydb.conf ]
 then
