c35875185f7afca3cbbc839a390f896684092574
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-12-31 14:11:04 +0100

    Migrate Crypto/pycryptopp to cryptography

--- pydb/crypto/__init__.py
+++ pydb/crypto/__init__.py
@@ -1,7 +1,2 @@
-# determine if pycryptopp or PyCrypto is available and set the aes ctr interface class accordingly
-try:
-    from pydb.crypto import aesctrpycryptopp
-    AesCtr = aesctrpycryptopp.AesCtrPycryptopp
-except ImportError:
-    from pydb.crypto import aesctrpycrypto
-    AesCtr = aesctrpycrypto.AesCtrPyCrypto
+from pydb.crypto import aesctrcryptography
+AesCtr = aesctrcryptography.AesCtrCryptography
